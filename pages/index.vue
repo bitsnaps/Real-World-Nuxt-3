320211f1fb4f4a4b7ef2f846714cafae8c9e4778
Ibrahim.H <bitsnaps@yahoo.fr>
2024-05-10 15:06:31 +0100

    update deps

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import type { Post } from '~/data/posts'
+import { getPostsUrl } from '~/data/posts'
+
+const { data: posts } = await useFetch<Post[]>(getPostsUrl())
+
+</script>
+
 <template>
-  <h1>Hello World</h1>
+  <main>
+    <h1>Nuxt 3 Blog {{ posts?.length }}</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+      {{ post.title }}
+</li>
+    </ul>
+  </main>
 </template>
