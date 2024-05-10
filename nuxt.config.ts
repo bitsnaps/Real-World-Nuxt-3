b2d6044a689a4759a7d18c5bd3dcd3a400a28b09
Ibrahim.H <bitsnaps@yahoo.fr>
2024-05-10 17:49:17 +0100

    hybride rendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  // Hybride rendering rules:
+  routeRules: {
+    '/': { ssr: true },
+    '/categories': { prerender: true },
+    '/categories/*': { swr: 300 },
+    '/posts/*': { swr: 60 }
+  }
 })
